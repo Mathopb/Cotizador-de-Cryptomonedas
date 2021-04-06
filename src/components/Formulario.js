7631b3f85ca62d2745c5248adf24fe21f9172a38
Mathopb <matho.pignone@gmail.com>
2021-04-06 16:22:06 -0300

    v1
    Un Cotizador de Criptomonedas.
    
    Sin los PropType

---
+++ src/components/Formulario.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState} from 'react';
+import Error from './Error'
+import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
+
+const Boton = styled.input`
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66a2fe;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color: #fff;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #326ac0;
+        cursor: pointer;
+    }
+`;
+
+const Formulario = ({guardarMoneda, guardarCriptomoneda}) => {
+
+    //State del Listado de Criptomonedas
+    const [listacripto, guardarCriptomonedas] = useState([]);
+    const [error, guardarError] = useState(false);
+
+    const MONEDAS = [
+        { codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
+        { codigo: 'MXN', nombre: 'Peso Mexicano'},
+        { codigo: 'EUR', nombre: 'Euro'},
+        { codigo: 'GBP', nombre: 'Libra Esterlina'},
+    ]
+
+    //Utilizar useMoneda
+    const [moneda, SelectMoneda] = useMoneda('Elige tu Moneda', '', MONEDAS);
+
+    //Utilizar useCriptomoneda
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Cryptomoneda', '', listacripto);
+
+    //Ejecutar llamado a la API
+    useEffect(() => {
+        const consutlarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+
+            const resultado = await axios.get(url);
+
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consutlarAPI();
+    }, []);
+
+    //Cuando el usuario hace submit
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        //Validar si ambos campos estan llenos
+        if(moneda === '' || criptomoneda === '') {
+            guardarError(true);
+            return;
+        }
+
+        //Pasar los datos al componente principal
+        guardarError(false);
+        guardarMoneda(moneda);
+        guardarCriptomoneda(criptomoneda);
+    }
+
+    return ( 
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {error
+                ?   <Error mensaje="Todos los campos son Obligatorios"/>
+                : null
+            }
+            
+            <SelectMoneda 
+
+            />
+
+            <SelectCripto 
+            
+            />
+
+            <Boton 
+                type="submit"
+                value="Calcular"
+            />
+        </form>
+     );
+}
+ 
+export default Formulario;
